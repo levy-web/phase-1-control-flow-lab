4f06f4f4647ac873e273b036e9358c7a20054cc2
levy <lnjiru43@gmail.com>
2022-11-23 15:24:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
-}
+  
+  if (ride <= 400){
+    return 'This one is on me!'
+  }else if (ride > 2000 && ride <=2500){
+    return 'I will gladly take your thirty bucks.'
+  }else if(ride > 2500 ){
+    return 'No can do.'
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
+console.log(scuberGreetingForFeet(2501))
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city === "NYC"){
+    return "Ok, sounds good."
+  }else return "No go."
 }
+console.log(ternaryCheckCity("NY"))
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip){
+
+    case "generous":
+      return "Thank you so much."
+      break;
+
+    case "not as generous" :
+      return "Thank you."
+      break;
+
+    default:
+      return "Bye."
+      break;  
+
+
+
+
+  }
+  
+}
+console.log(switchOnCharmFromTip("generus"))
